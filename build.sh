ddfe0391584527849fb69983d91dcc5ae04cc2b5
itsgreatness <dzhao650@gmail.com>
2024-01-13 12:51:45 -0600

    Build scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 npx terser src/src.js -o dist/dist.js -c -m
+cat /dev/null > index.html
+echo "<a href='javascript:\c" >> index.html
+cat dist/dist.js >> index.html
+echo "'>Cybertanks</a>" >> index.html
